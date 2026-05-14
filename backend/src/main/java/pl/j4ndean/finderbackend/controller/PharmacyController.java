bf093bc56c6594ce7f699f264d07293ea4314cdc
J4n <jandziekan333@gmail.com>
2026-05-14 19:02:34 +0200

    fix: allow CORS from Vercel domains

--- backend/src/main/java/pl/j4ndean/finderbackend/controller/PharmacyController.java
+++ backend/src/main/java/pl/j4ndean/finderbackend/controller/PharmacyController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/pharmacies")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(originPatterns = {"http://localhost:*", "https://*.vercel.app"})
 public class PharmacyController {
 
     private final PharmacyService pharmacyService;
